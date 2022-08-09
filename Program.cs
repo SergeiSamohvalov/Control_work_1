a0f40590c67e1a833ac2190d801d49d7556ddd5b
sergei sam <ssi1970@mail.ru>
2022-08-09 15:22:02 +0700

    solution added

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿string[] array;
+Console.WriteLine("Введите массив строк через пробел");
+string arr = Console.ReadLine()!;
+array = arr!.Split(' ');
+Console.WriteLine();
+Console.WriteLine("Начальный массив: [" + string.Join(", ", array) + "]");
+var result = new string[array.Length];
+var arrayelementsize = 0;
+foreach (var value in array)
+{
+    if (value.Length <= 3)
+    {
+        result[arrayelementsize] = value;
+        arrayelementsize++;
+    }
+}
+Console.WriteLine("Результат:");
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, arrayelementsize));
